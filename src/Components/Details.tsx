6fdadad9d5104c250a0b761ce195aa748cb3b6b5
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-04-23 21:21:04 +0400

    creaet-details-component

--- src/Components/Details.tsx
+++ src/Components/Details.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Details() {
-  return <div>Details</div>;
+  return (
+    <div>
+      <p>Description</p>
+      <p></p>
+      <p>Humidity</p>
+      <p></p>
+      <p>Pressure</p>
+      <p></p>
+      <p>Wind</p>
+      <p></p>
+      <p>Sunrise</p>
+      <p></p>
+      <p>Sunset</p>
+      <p></p>
+    </div>
+  );
 }
 
 export default Details;
