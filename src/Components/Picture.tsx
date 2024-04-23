2990bb642b4417f375db1fff0cf817363b52eaa2
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-04-23 21:14:18 +0400

    create-picture-component

--- src/Components/Picture.tsx
+++ src/Components/Picture.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Picture() {
-  return <div>Picture</div>;
+  return (
+    <div>
+      <img alt="weather"></img>
+      <h1>Temperature</h1>
+      <h2>City</h2>
+      <h5>Date</h5>
+      <img alt="icon"></img>
+      <h5>Weather</h5>
+    </div>
+  );
 }
 
 export default Picture;
