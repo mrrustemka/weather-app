0c67da1ce4b488315927da3873efc284b07b2c80
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-04-24 21:49:42 +0400

    add-se-effect

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useEffect } from "react";
+// import { WEATHER_API_KEY} from '../'
+// import process.e from "process";
+import "../react-app-env";
 
 function Search() {
+  // useEffect(function () {
+  //   async function getWeather() {
+  //     const res = await fetch(
+  //       `https://api.openweathermap.org/data/2.5/weather?q=Batumi&lang=en&units=metric&appid=${api_key}`
+  //     );
+  //     const data = await res.json();
+  //     console.log(data);
+  //   }
+  //   getWeather();
+  // }, []);
+
   return (
     <div>
       <input placeholder="Enter City"></input>
